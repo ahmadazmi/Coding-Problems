ace0dcb6fc4d3b0a0aeae1703e6553ede1eb1ada
Ahmad Azmi <ahmad.azmi@stonybrook.edu>
2022-05-08 03:35:27 -0400

    LeetCode problem: two-sum Added

---
+++ LeetCode/two-sum/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int i = 0, j = 1;
+        while (i < nums.length) {
+            if (nums[i] + nums[j] == target) break;
+            if (j < nums.length-1) {j++;}
+            else {i++; j = i+1;}
+        }
+        return new int[] {i, j};
+    }
+}
