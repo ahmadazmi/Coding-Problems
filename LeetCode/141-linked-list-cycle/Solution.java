b4684d047b7ded55b8830897bbcd43b3209806d3
Ahmad Azmi <ahmad.azmi@stonybrook.edu>
2022-05-31 04:26:24 -0400

    Added class dependancy for problems

--- LeetCode/141-linked-list-cycle/Solution.java
+++ LeetCode/141-linked-list-cycle/Solution.java
@@ -1,14 +1,13 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
 public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode traverseOneNode = head;
