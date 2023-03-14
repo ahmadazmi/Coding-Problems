79dfe0d3073c4c309226ac00918e2c099ee6b835
Ahmad Azmi <ahmad.azmi@stonybrook.edu>
2023-03-13 20:18:03 -0400

    Added LC 206

---
+++ LeetCode/206-reverse-linked-list/Solution.java
@@ -0,0 +1,22 @@
+class ListNode {
+	int val;
+	ListNode next;
+	ListNode() {}
+	ListNode(int val) { this.val = val; }
+	ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode prev = null, current = head, next = head.next;
+        while (next != null) {
+            current.next = prev;
+            prev = current;
+            current = next;
+            next = next.next;
+        }
+        current.next = prev;
+        return current;
+    }
+}
